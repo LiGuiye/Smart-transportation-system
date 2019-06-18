a00780f3d14acd3cacb581c17b0515e551c0d48e
LiGuiye <1171116470@qq.com>
2019-06-18 16:32:39 +0800

    代码精简，添加控件

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -24,7 +24,12 @@ view = new ol.View({
 map = new ol.Map({
 	target: "mapDiv",
 	layers: [TiandiMap_vec, TiandiMap_img],
-	view: view
+	view: view,
+	controls: ol.control.defaults({
+		attribution: false,
+		rotate: false,
+		zoom: false
+	})
 });
 /////////////////// 功能封装//////////////////////////////
 // 底图切换
@@ -32,17 +37,51 @@ function sl() {
 	TiandiMap_vec.setVisible(true);
 	TiandiMap_img.setVisible(false);
 }
+
 function yx() {
 	TiandiMap_vec.setVisible(false);
 	TiandiMap_img.setVisible(true);
 }
-
+//控件
+var mousePositionControl = new ol.control.MousePosition({
+	coordinateFormat: ol.coordinate.createStringXY(4),
+	className: 'custom-mouse-position',
+	target: document.getElementById('mouse-position'),
+	undefinedHTML: '&nbsp;'
+});
+map.addControl(mousePositionControl);
+//单击放大按钮功能
+$('#zoomin').click(function() {
+	var view = map.getView();
+	var zoom = view.getZoom();
+	view.setZoom(zoom + 1);
+});
+//单击缩小按钮功能
+$('#zoomout').click(function() {
+	var view = map.getView();
+	var zoom = view.getZoom();
+	view.setZoom(zoom - 1);
+});
+//复位功能（复位到初始状态）
+//地图视图的初始参数
+var viewRes = map.getView();
+var zoomRes = viewRes.getZoom();
+var centerRes = viewRes.getCenter();
+$('#restore').click(function() {
+	//获取地图视图
+	var view = map.getView();
+	//设置初始中心点
+	view.setCenter(centerRes);
+	//设置初始缩放级数
+	view.setZoom(zoomRes);
+});
 //左侧目录树查询生成及联动
 var docCatalog = new Zondy.Service.Catalog.MapDoc({
 	ip: ip,
 	port: port,
 });
 docCatalog.getMapDocList(getMapDocSuccess, getInfoError);
+
 function getMapDocSuccess(data) {
 	if (data.DOCNames.length > 0) {
 		var create = '<li class="lis">';
@@ -57,9 +96,11 @@ function getMapDocSuccess(data) {
 		console.log("没看到地图文档")
 	}
 }
+
 function getInfoError(data) {
 	alert("请求失败，请检查参数！");
 }
+
 function addMapDoc(val) {
 	mapdoc = new Zondy.Map.Doc(val, val, {
 		ip: ip,
@@ -67,9 +108,11 @@ function addMapDoc(val) {
 	});
 	map.addLayer(mapdoc);
 }
+
 function changeExtent(docName) {
 	getMapInfo(docName);
 }
+
 function getMapInfo(docName) {
 	var docCatalog = new Zondy.Service.Catalog.MapDoc({
 		ip: ip,
@@ -80,6 +123,7 @@ function getMapInfo(docName) {
 	docCatalog.getMapInfo(getMapInfoSuccess, false, false, getInfoError);
 }
 var list = '';
+
 function getMapInfoSuccess(data) {
 	var extent = data.range;
 	extent = extent.split(',');
@@ -99,26 +143,28 @@ function getMapInfoSuccess(data) {
 		console.log("没看到图层")
 	}
 }
+
 function selectShow() {
 	var selected = [];
 	$(".menu_list ul li .content .zcd").click(function() {
 
-		if ($(this).context.classList[1] == "removes" ) {
+		if ($(this).context.classList[1] == "removes") {
 			$(this).removeClass("removes");
-			removeSelect(selected,$(this).context.id);
-		} else{
+			removeSelect(selected, $(this).context.id);
+		} else {
 			$(this).addClass("removes");
 			selected.push($(this).context.id);
 		}
 		console.log(selected.toString());
 		var result = selected.toString();
-		if(selected.toString() == ''){
+		if (selected.toString() == '') {
 			result = 111111111
 		}
 		mapdoc.setLayerStatus(result, "show");
 		mapdoc.refresh();
 	});
 }
+
 function removeSelect(arry, val) {
 	for (var key = 0; key < arry.length; key++) {
 		if (arry[key] == val) {
